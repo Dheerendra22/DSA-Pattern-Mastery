d7699ba971dcca7b53ba8b79c00e1493d4f9cff2
Dheerendra Mishra <dheerendramishra64646@gmail.com>
2026-08-31 12:05:28 +0530

    Updated files

---
+++ BackTracking/P2.py
@@ -0,0 +1,17 @@
+# Time Complexity O(nxn!)
+# Space Complexity O(n)
+def permutation(nums): 
+    n=len(nums)
+    result=[]
+    def perm(i):
+        if i==n:
+            result.append(nums[::])
+            return
+        for j in range(i,n):
+            nums[i],nums[j]=nums[j],nums[i]
+            perm(i+1)
+            nums[i],nums[j]=nums[j],nums[i]
+    perm(0)
+    return result 
+for l in permutation([1,2,3]):
+    print(l)
