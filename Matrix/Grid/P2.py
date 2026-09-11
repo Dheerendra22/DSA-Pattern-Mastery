a0e6e0cdca0900b5358adcd5e0ec6ee3c953b17f
Dheerendra Mishra <pransu@Dheerendras-MacBook-Air.local>
2026-09-11 16:59:13 +0530

    Updates from macbook

---
+++ Matrix/Grid/P2.py
@@ -0,0 +1,32 @@
+"""
+Problem Description
+Given an m x n binary matrix filled with 0s and 1s, find the largest square containing only 1s and return its area.
+Example 1
+Input:
+matrix = [
+    ["1","0","1","0","0"],
+    ["1","0","1","1","1"],
+    ["1","1","1","1","1"],
+    ["1","0","0","1","0"]
+]
+Output:
+4
+Explanation:
+The largest square has a side length of 2.
+Therefore:
+Area = side × side
+     = 2 × 2
+     = 4
+Example 2
+Input:
+matrix = [
+    ["0","1"],
+    ["1","0"]
+]
+Output:
+1
+Because there is no 2 × 2 square containing only 1s. The largest possible square is:
+1 × 1
+So:
+Area = 1 × 1 = 1
+"""
